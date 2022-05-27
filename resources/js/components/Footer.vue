c9545c313b5bf613b8adc1ff266336189ddaa3b2
Andrey Helldar <helldar@ai-rus.com>
2022-05-27 18:38:11 +0300

    Fill the footer

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,5 +1,22 @@
 <template>
-    <v-footer>
-        This is a footer
+    <v-footer padless>
+        <v-col cols="12" class="text-center">
+            {{ new Date().getFullYear() }} - <strong>{{ title }}</strong>
+        </v-col>
     </v-footer>
 </template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+    setup() {
+        const store = useStore();
+
+        return {
+            title: computed(() => store.getters['meta/title'])
+        };
+    }
+};
+</script>
