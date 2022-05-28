488a9c66d8c372b3dd7a68c10b0e17938ec4c764
Andrey Helldar <helldar@ai-rus.com>
2022-05-28 13:57:43 +0300

    Added ngrok shell file

---
+++ ngrok.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#
+# This file is part of the "Volunteers CRM" project.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author Andrey Helldar <helldar@dragon-code.pro>
+#
+# @copyright 2022 Andrey Helldar
+#
+# @license MIT
+#
+# @see https://github.com/volunteers-crm
+#
+
+ngrok http laravel.test:3000
