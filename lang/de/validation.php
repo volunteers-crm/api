e2855699db1865a72d0415485edba91b212130fb
Andrey Helldar <helldar@dragon-code.pro>
2022-10-20 02:04:10 +0300

    Enabled strict types

--- lang/de/validation.php
+++ lang/de/validation.php
@@ -13,6 +13,21 @@
  * @see https://github.com/volunteers-crm
  */
 
+declare(strict_types=1);
+
+/**
+ * This file is part of the "Volunteers CRM" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@dragon-code.pro>
+ * @copyright 2022 Andrey Helldar
+ * @license MIT
+ *
+ * @see https://github.com/volunteers-crm
+ */
+
 return [
     'accepted'             => 'Dieses Feld muss akzeptiert werden.',
     'accepted_if'          => 'Dieses Feld muss akzeptiert werden, wenn :other :value ist.',
