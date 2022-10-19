44346ef223d71b650286a69498e779ecac8aee7e
Andrey Helldar <helldar@dragon-code.pro>
2022-10-20 02:12:53 +0300

    Prepare to work

--- lang/de/auth.php
+++ lang/de/auth.php
@@ -13,21 +13,6 @@
  * @see https://github.com/volunteers-crm
  */
 
-declare(strict_types=1);
-
-/**
- * This file is part of the "Volunteers CRM" project.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @author Andrey Helldar <helldar@dragon-code.pro>
- * @copyright 2022 Andrey Helldar
- * @license MIT
- *
- * @see https://github.com/volunteers-crm
- */
-
 return [
     'failed'   => 'Diese Kombination aus Zugangsdaten wurde nicht in unserer Datenbank gefunden.',
     'password' => 'Das Passwort ist falsch.',
--- routes/api.php
+++ routes/api.php
@@ -27,12 +27,3 @@ declare(strict_types=1);
  *
  * @see https://github.com/volunteers-crm
  */
-
-use App\Http\Controllers\Api\IndexController;
-use App\Http\Controllers\Api\Users\UserController;
-
-app('router')
-    ->middleware('auth:sanctum')
-    ->get('/user', [UserController::class, 'me']);
-
-app('router')->get('/', IndexController::class);
