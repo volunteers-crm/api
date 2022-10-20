52e2157f38a0fbe04d0a485112f15f8405278e02
Andrey Helldar <helldar@dragon-code.pro>
2022-10-20 23:44:26 +0300

    Removed duplicated copyrights

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,19 +15,6 @@
 
 declare(strict_types=1);
 
-/**
- * This file is part of the "Volunteers CRM" project.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @author Andrey Helldar <helldar@dragon-code.pro>
- * @copyright 2022 Andrey Helldar
- * @license MIT
- *
- * @see https://github.com/volunteers-crm
- */
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
