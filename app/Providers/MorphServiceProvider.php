81b432f602dc9581acf0d8346308edad552c69a5
Andrey Helldar <helldar@dragon-code.pro>
2022-10-23 21:30:43 +0300

    Added morph map

---
+++ app/Providers/MorphServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of the "Volunteers CRM" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@dragon-code.pro>
+ * @copyright 2022 Andrey Helldar
+ * @license MIT
+ *
+ * @see https://github.com/volunteers-crm
+ */
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use App\Models\Appeal;
+use App\Models\Bot;
+use App\Models\Channel;
+use App\Models\Chat;
+use App\Models\Page;
+use App\Models\Role;
+use App\Models\Social;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\ServiceProvider;
+
+class MorphServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        Relation::enforceMorphMap([
+            'appeal'  => Appeal::class,
+            'bot'     => Bot::class,
+            'channel' => Channel::class,
+            'chat'    => Chat::class,
+            'page'    => Page::class,
+            'role'    => Role::class,
+            'social'  => Social::class,
+            'user'    => User::class,
+        ]);
+    }
+}
