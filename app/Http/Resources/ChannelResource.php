cae8e065fa3076c0205d2ad414fa9a41c2daece2
Andrey Helldar <helldar@dragon-code.pro>
2022-11-04 02:48:35 +0300

    Disabled the ability to delete someone else's channel

--- app/Http/Resources/ChannelResource.php
+++ app/Http/Resources/ChannelResource.php
@@ -18,6 +18,8 @@ declare(strict_types=1);
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\MissingValue;
+use Illuminate\Support\Facades\Auth;
 
 /** @mixin \App\Models\Channel */
 class ChannelResource extends JsonResource
@@ -33,7 +35,18 @@ class ChannelResource extends JsonResource
                 'closed' => $this->appeals_closed,
             ],
 
+            'can_delete' => $this->canDelete(),
+
             'bot' => BotResource::make($this->whenLoaded('bot')),
         ];
+    }
+
+    protected function canDelete(): bool
+    {
+        if ($this->whenLoaded('bot') instanceof MissingValue) {
+            return false;
+        }
+
+        return $this->bot->owner_id === Auth::id();
     }
 }
