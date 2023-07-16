3593074f570de559e1291ed0bec8a64596ee9d11
Andrey Helldar <helldar@dragon-code.pro>
2023-07-16 23:06:27 +0300

    Update files

--- lang/ru/passwords.php
+++ lang/ru/passwords.php
@@ -1,23 +1,10 @@
 <?php
 
-/**
- * This file is part of the "Volunteers CRM" project.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @author Andrey Helldar <helldar@dragon-code.pro>
- * @copyright 2023 Andrey Helldar
- * @license MIT
- *
- * @see https://github.com/volunteers-crm
- */
-
 declare(strict_types=1);
 
 return [
-    'reset'     => 'Ваш пароль был сброшен!',
-    'sent'      => 'Ссылка на сброс пароля была отправлена!',
+    'reset'     => 'Ваш пароль был сброшен.',
+    'sent'      => 'Ссылка на сброс пароля была отправлена.',
     'throttled' => 'Пожалуйста, подождите перед повторной попыткой.',
     'token'     => 'Ошибочный код сброса пароля.',
     'user'      => 'Не удалось найти пользователя с указанным электронным адресом.',
